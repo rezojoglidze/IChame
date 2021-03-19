e0e512e5cb7ecd532780258582735e2e8372ed7f
Rezo Joglidze <rezojoglidze7@gmail.com>
2021-03-19 15:41:54 +0400

    added main screen cell

---
+++ IChame/App Modules/Menu/MenuScreen/MenuScreenCell/MenuScreenCell.swift
@@ -0,0 +1,30 @@
+//
+//  MenuScreenCell.swift
+//  IChame
+//
+//  Created by Rezo Joglidze on 3/19/21.
+//  Copyright © 2021 Rezo Joglidze. All rights reserved.
+//
+
+import UIKit
+
+class MenuScreenCell: UITableViewCell {
+
+    @IBOutlet weak var menuImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func fill(imgUrl: String, title: String) {
+        self.titleLbl.text = title
+    }
+}
--- IChame/App Modules/Menu/MenuScreen/MenuScreenViewController.swift
+++ IChame/App Modules/Menu/MenuScreen/MenuScreenViewController.swift
@@ -13,6 +13,8 @@ class MenuScreenViewController: UIViewController {
     
     var viewModel: MenuScreenViewModelProtocol!
     
+    @IBOutlet private weak var tableView: UITableView!
+    
     static func instantiate(strongRouter: StrongRouter<MenuRoute>) -> Self {
         let viewController = ScreensAssembly.shared.container.resolve(Self.self, argument: strongRouter) ?? .init()
         return viewController
@@ -22,5 +24,24 @@ class MenuScreenViewController: UIViewController {
         super.viewDidLoad()
         
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func setupTableView() {
+        tableView.register(UINib(nibName: String(describing: MenuScreenCell.self), bundle: nil), forCellReuseIdentifier: String(describing: MenuScreenCell.self))
+        tableView.tableFooterView = UIView()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension MenuScreenViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MenuScreenCell.self)) as! MenuScreenCell
+        cell.fill(imgUrl: "", title: "ცხელი საჭმელი")
+        return cell
     }
 }
